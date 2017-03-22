39ae31dbe3e464ff4890868a3c932c445ca295d5
bastidas <alexander.bastidas.fry@gmail.com>
2017-03-21 17:10:58 -0700

    Checks if valid stock symbol entered

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
 from flask import Flask, render_template, request, redirect
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-  return redirect('/index')
+#tickers = pd.read_csv('https://s3.amazonaws.com/quandl-static-content/Ticker+CSV%27s/WIKI_tickers.csv')
+tickers = pd.read_csv('tickers.csv')
 
-@app.route('/index')
+ticker_symbols = tickers['quandl code'].values
+
+
+#@app.route('/')
+#def main():
+#  return redirect('/index')
+
+#@app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+    print("request.method:", request.method)
+    print("request.form:", request.form)
+    if request.method == 'POST' and 'symbol' in request.form:
+        sym = request.form['symbol'].upper()
+        print('sym: ', sym)
+        #print('sym.upper: ', sym)
+        #if 'WIKI/' + sym.upper() in ticker_symbols:
+        if 'WIKI/' + sym in ticker_symbols:
+            try:
+                return render_template('index.html', place_holder="I know that symbol")
+            except:
+                return render_template('index.html', place_holder="Fail.")
+        else:
+            return render_template('index.html', place_holder="I do not know this symbol")
+    else:
+        print("first else...")
+        return render_template('index.html', place_holder="Please input a stock symbol...")
+
+#@app.route('/404')
+#def not_found():
+#    return render_template('404.html')
+
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
